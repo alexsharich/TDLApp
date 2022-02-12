71f9bdeadedfb14d8ab4a59e28d830b885e30039
alexsharich <alexsharich@gmail.com>
2022-02-12 14:34:16 +0100

    add typing of todolistAPI

--- src/api/todolistApi.ts
+++ src/api/todolistApi.ts
@@ -8,17 +8,43 @@ const instance = axios.create({
     }
 })
 export const todolistAPI = {
-    getTodos() {
-        return instance.get('todo-lists')
+    getTodolists() {
+        return instance.get<Array<TodolistType>>('todo-lists')
     },
     createTodolis(title: string) {
-        return instance.post(`todo-lists/`, { title: 'todolist in API' })
+        return instance.post<CreateTodolistResponseType>(`todo-lists/`, { title: 'todolist in API' })
     },
     removeTodolis(id: string) {
-        return instance.delete(`todo-lists/${id}`)
+        return instance.delete<DeleteTodolistResponseType>(`todo-lists/${id}`)
     },
-    updateTodolis(id: string, title: string) {
-        return instance.put(`todo-lists/${id}`, { title: 'updated title' })
+    updateTodolist(id: string, title: string) {
+        return instance.put<UpdateTodolistType>(`todo-lists/${id}`, { title: 'updated title' })
     },
 
+}
+type TodolistType = {
+    id: string
+    addedDate: string
+    order: number
+    title: string
+}
+type CreateTodolistResponseType = {
+    resultCode: number
+    messages: Array<string>
+    fieldsError: Array<string>
+    data: {
+        item: TodolistType
+    }
+}
+type UpdateTodolistType = {
+    resultCode: number
+    messages: Array<string>
+    fieldsErrors: Array<string>
+    data: {} 
+}
+type DeleteTodolistResponseType = {
+    resultCode: number
+    messages: Array<string>
+    fieldsErrors: Array<string>
+    data: {}
 }
