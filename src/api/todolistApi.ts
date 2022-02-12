e9cc4b0ee58c1a853a41fa68290468b9ef331f9e
alexsharich <alexsharich@gmail.com>
2022-02-12 15:36:57 +0100

    add getTasks,createTask,deleteTask,updateTask in todolistAPI

--- src/api/todolistApi.ts
+++ src/api/todolistApi.ts
@@ -15,14 +15,26 @@ export const todolistAPI = {
         return instance.post<ResponseType<{ item: TodolistType }>>(`todo-lists/`, { title: 'todolist in API' })
     },
     removeTodolis(id: string) {
-        return instance.delete<ResponseType<{}>>(`todo-lists/${id}`)
+        return instance.delete<ResponseType>(`todo-lists/${id}`)
     },
     updateTodolist(id: string, title: string) {
-        return instance.put<ResponseType<{}>>(`todo-lists/${id}`, { title: 'updated title' })
+        return instance.put<ResponseType>(`todo-lists/${id}`, { title: 'updated title' })
     },
+    getTasks(todolistId: string) {
+        return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
+    },
+    createTask(todolistId: string, title: string) {
+        return instance.post(`/todo-lists/${todolistId}/tasks`)
+    },
+    deleteTask(todolistId: string, taskId: string) {
+        return instance.delete<ResponseType>(`/todo-lists/${todolistId}/tasks/${taskId}`)
+    },
+    updateTask(todolistId: string, taskId: string, model: UpdateTaskType) {
+        return instance.put(`/todo-lists/${todolistId}/tasks/${taskId}`)
+    }
 
 }
-type ResponseType<D> = {
+type ResponseType<D = {}> = {
     resultCode: number
     messages: Array<string>
     fieldsError: Array<string>
@@ -33,4 +45,30 @@ type TodolistType = {
     addedDate: string
     order: number
     title: string
+}
+type GetTasksResponse = {
+    items: Array<TaskType>
+    totalCount: number
+    error: string | null
+}
+type TaskType = {
+    description: string
+    title: string
+    completed: boolean
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
+    id: string
+    todoListId: string
+    order: number
+    addedDate: number
+}
+type UpdateTaskType = {
+    title: string
+    description: string
+    status: number
+    priority: number
+    startDate: string
+    deadLine: string
 }
